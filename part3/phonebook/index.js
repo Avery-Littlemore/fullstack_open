f897768e1e24ec45437b311303d0e802535165ed
Avery Littlemore <avery.d.littlemore@gmail.com>
2024-03-21 14:42:56 -0400

    mar21 upload

---
+++ part3/phonebook/index.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.static('dist'))
+
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>
+      Phonebook has info for ${persons.length} people<br/>
+      ${new Date()}
+    </p>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).send({ error: 'invalid id provided' })
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  // const maxId = persons.length > 0
+  //   ? Math.max(...persons.map(n => n.id))
+  //   : 0
+  // return maxId + 1
+
+  return Math.ceil(Math.random() * 1000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name must be provided' 
+    })
+  } else if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number must be provided' 
+    })
+  } else if (persons.filter(person => {
+              return person.name === body.name
+            }).length > 0) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
