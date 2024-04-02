cef2fa1a91e2f6e0c587db6510847883b4a24c69
Avery Littlemore <avery.d.littlemore@gmail.com>
2024-04-02 10:05:12 -0400

    anecdotes app with browserRouter

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,79 +1,173 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link,
+  useParams, useNavigate,
+  useMatch
+} from 'react-router-dom'
 
 const Heading = ({ text }) => (
   <h1>{text}</h1>
 )
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+const Footer = ({ text }) => (
+  <p><em>{text}</em></p>
 )
 
-const VoteTally = ({ currentTally }) => {
-  return (
-    <p>has {currentTally} votes</p>
-  )
-}
+const Home = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => {
+        return <li key={anecdote.id}><Link to={`/anecdotes/${anecdote.id}`} >{anecdote.content}</Link></li>
+      })}
+    </ul>
+  </div>
+)
 
-const MostVotes = ({ votes, anecdotes }) => {
-  let maxValue = 0;
-  let maxIndex = 0;
-  const length = Object.keys(votes).length;
+const Create = ({ anecdotes, setAnecdotes, setNotification }) => {
+  const navigate = useNavigate()
 
-  for (let currentIndex = 0; currentIndex < length; currentIndex += 1) {
-    if (votes[currentIndex] > maxValue) {
-      maxValue = votes[currentIndex];
-      maxIndex = currentIndex;
-    }
-  }
+  const [content, setContent] = useState('') 
+  const [author, setAuthor] = useState('') 
+  const [url, setUrl] = useState('') 
 
-  return (
-    <>
-      <p>{anecdotes[maxIndex]}</p>
-      <VoteTally currentTally={maxValue} />
-    </>
-  )
-}
-
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 })
-
-  const selectRandom = () => {
-    const idx = Math.floor(Math.random() * anecdotes.length);
-    setSelected(idx);
-  }
-
-  const addVote = () => {
-    const copy = {...votes}
-    copy[selected] += 1;
-    // setVotes({...votes, [selected]: votes[selected] + 1});
-    setVotes({...copy});
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const id = Math.max(...anecdotes.map(anecdote => Number(anecdote.id))) + 1
+    setAnecdotes(anecdotes.concat({ id, content, author, url }))
+    setNotification(content)
+    navigate('/')
   }
 
   return (
     <div>
-      <Heading text="Anecdote of the day" />
-      {anecdotes[selected]}
-      <br />
-      <VoteTally currentTally={votes[selected]} />
-      <Button handleClick={addVote} text="vote" />
-      <Button handleClick={selectRandom} text="next anecdote" />
-      <Heading text="Anecdote with most votes" />
-      <MostVotes votes={votes} anecdotes={anecdotes} />
+      <h2>Create a new anecdote</h2>
+
+      <form onSubmit={onSubmit}>
+        content: <input
+          id='content'
+          value={content}
+          onChange={event => setContent(event.target.value)}
+        /><br/>
+        author: <input
+          id='author'
+          value={author}
+          onChange={event => setAuthor(event.target.value)}
+        /><br/>
+        url for more info: <input
+          id='url'
+          value={url}
+          onChange={event => setUrl(event.target.value)}
+        /><br/>
+        <button type="submit">create</button>
+      </form>
     </div>
+  )
+}
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(n => n.id === Number(id))
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <div>for more info see {anecdote.url}</div>
+    </div>
+  )
+}
+
+const Notification = ({ notification, setNotification }) => {
+  if (notification) {
+    setTimeout(() => {
+      setNotification(null)}
+    , 5000)
+    return (
+      <>
+        <p>A new anecdote {notification} created!</p>
+      </>
+    )
+  }
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      id: 1,
+      content: 'If it hurts, do it more often.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 2,
+      content: 'Adding manpower to a late software project makes it later!',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 3,
+      content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 4,
+      content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 5,
+      content: 'Premature optimization is the root of all evil.',
+      author: 'Avery',
+      url: 'avery.com',
+
+    },
+    {
+      id: 6,
+      content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 7,
+      content: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+    {
+      id: 8,
+      content: 'The only way to go fast, is to go well.',
+      author: 'Avery',
+      url: 'avery.com',
+    },
+  ])
+
+  const [notification, setNotification] = useState(null)
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <Heading text="Software Anecdotes" />
+      <div>
+        <Link style={padding} to="/">anecdotes</Link>
+        <Link style={padding} to="/create">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+      </div>
+
+      <Notification notification={notification} setNotification={setNotification} />
+
+      <Routes>
+        <Route path="/create" element={<Create anecdotes={anecdotes} setAnecdotes={setAnecdotes} setNotification={setNotification} />} />
+        <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
+        <Route path="/" element={<Home anecdotes={anecdotes} />} />
+      </Routes>
+
+      <Footer text="Anecdote app created by Avery Littlemore" />
+    </Router>
   )
 }
 
